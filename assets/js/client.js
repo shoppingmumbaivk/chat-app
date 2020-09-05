14adbef86b735782074057d1844b194d9bcb2eee
shoppingmumbaivk <69787197+shoppingmumbaivk@users.noreply.github.com>
2020-09-05 20:57:35 +0530

    Add files via upload

---
+++ assets/js/client.js
@@ -0,0 +1,36 @@
+const socket = io('http://localhost:8000')
+
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".message-container")
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right');
+})
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on('leave', name => {
+    append(`${name} left the chat`, 'left');
+})
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
